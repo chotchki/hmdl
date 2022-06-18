73bcf0796c53fb02b2af4be3a233d6bd2ba8fe55
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2022-06-18 16:13:03 -0400

    Added in a react app frontend, testing embedding now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
+    body::{boxed, Full},
+    handler::Handler,
+    http::{header, StatusCode, Uri},
+    response::{Html, IntoResponse, Response},
+    routing::{get, post, Router},
+    Json,
 };
 use git_version::git_version;
+use rust_embed::RustEmbed;
 use std::net::SocketAddr;
 use tracing_subscriber::{
     fmt, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
@@ -29,10 +32,19 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(index_handler))
+        //Have to explictly add mappings for everything top level so the router doesn't have conflicts
+        .route("/asset-manifest.json", static_handler.into_service())
+        .route("/favicon.ico", static_handler.into_service())
+        .route("/index.html", get(index_handler))
+        .route("/logo192.png", static_handler.into_service())
+        .route("/logo512.png", static_handler.into_service())
+        .route("/manifest.json", static_handler.into_service())
+        .route("/robots.txt", static_handler.into_service())
+        .route("/static/*file", static_handler.into_service())
+        .fallback(get(not_found));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -40,7 +52,53 @@ async fn main() {
         .unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World! 5"
+// We use static route matchers ("/" and "/index.html") to serve our home
+// page.
+async fn index_handler() -> impl IntoResponse {
+    static_handler("/index.html".parse::<Uri>().unwrap()).await
+}
+
+// We use a wildcard matcher ("/dist/*file") to match against everything
+// within our defined assets directory. This is the directory on our Asset
+// struct below, where folder = "examples/public/".
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let mut path = uri.path().trim_start_matches('/').to_string();
+
+    StaticFile(path)
+}
+
+// Finally, we use a fallback route for anything that didn't match.
+async fn not_found() -> Html<&'static str> {
+    Html("<h1>404</h1><p>Not Found</p>")
+}
+
+// Static Example from here: https://github.com/pyrossh/rust-embed/blob/master/examples/axum.rs
+#[derive(RustEmbed)]
+#[folder = "hearthstone-frontend/build"]
+struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let body = boxed(Full::from(content.data));
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                Response::builder()
+                    .header(header::CONTENT_TYPE, mime.as_ref())
+                    .body(body)
+                    .unwrap()
+            }
+            None => Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(boxed(Full::from("404")))
+                .unwrap(),
+        }
+    }
 }
